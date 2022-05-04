b7528e8ce2feb9a5d379257d166cf6e0c14f0d80
KURANDY Eva <kuraeva@ndlp.fr>
2022-05-04 16:57:25 +0200

    last commit

--- app.py
+++ app.py
@@ -36,15 +36,13 @@ def get_db_connection():
 def get_post(post_id):
     myconn = get_db_connection()
     mycursor = myconn.cursor(dictionary=True)
-    mycursor.execute('SELECT * FROM post WHERE id =%s',
-(post_id,))
+    mycursor.execute('SELECT * FROM post WHERE id =%s', (post_id,))
     row = mycursor.fetchone()  
     myconn.close()
     if row is None:
         abort(404)
     else:
-        post = Post(row['id'], row['created'], row['title'],
-row['content'])
+        post = Post(row['id'], row['created'], row['title'], row['content'])
         return post
 
 @app.route('/<int:post_id>')
@@ -54,4 +52,46 @@ def post(post_id):
     
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            myconn = get_db_connection()
+            mycursor = myconn.cursor(dictionary=True)
+            mycursor.execute('INSERT INTO post (created, title, content) VALUES (%s, %s, %s)', (datetime.datetime.now(), title, content))
+            myconn.commit()
+            myconn.close()
+            return redirect(url_for('index'))
+
     return render_template('create.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash('Title is required!')
+        else:
+            myconn = get_db_connection()
+            mycursor = myconn.cursor(dictionary=True)
+            mycursor.execute('UPDATE post SET title = %s, content = %s WHERE id = %s',  (title, content, id))
+            myconn.commit()
+            myconn.close()
+            return redirect(url_for('index'))
+    return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    myconn = get_db_connection()
+    mycursor = myconn.cursor(dictionary=True)
+    mycursor.execute('DELETE FROM post WHERE id = %s', (id,))
+    myconn.commit()
+    myconn.close()
+    flash('"{}" was successfully deleted!'.format(post.title))
+    return redirect(url_for('index'))
