133e6c8427b3bb9fbaf96966191c28d507acd82f
KURANDY Eva <kuraeva@ndlp.fr>
2022-05-11 17:01:23 +0200

    new commit

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from flask import Flask, render_template, request, url_for,flash, redirect
 from werkzeug.exceptions import abort
 from post import Post
 import secrets, datetime
+from flask_login import LoginManager
 
+login_manager = LoginManager()
 app = Flask(__name__)
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 @app.route('/')
 def index():    
@@ -95,3 +98,29 @@ def delete(id):
     myconn.close()
     flash('"{}" was successfully deleted!'.format(post.title))
     return redirect(url_for('index'))
+    
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
